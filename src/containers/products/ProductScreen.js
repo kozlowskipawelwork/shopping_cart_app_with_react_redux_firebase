97f1e955698da740dcce8aa9186dbbb4f291e55f
ESPP-ZDT <toplelfaget@gmail.com>
2022-08-19 17:02:00 +0200

    updated the styles in ProductScreen

--- src/containers/products/ProductScreen.js
+++ src/containers/products/ProductScreen.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import {products} from '../../constants/products'
 import ProductItem from '../../components/product/ProductItem'
+import { PageHeading, ProductContainerStyle } from '../../styles/ProductScreen'
 
 const ProductScreen = () => {
     return (
         <div>
-            <h1>Products</h1>
+            <PageHeading primary>Products</PageHeading>
+            <ProductContainerStyle primary>
             {products.map((item) => (
                 <ProductItem item={item} />
               ))}
+            </ProductContainerStyle>
         </div>
 
 
