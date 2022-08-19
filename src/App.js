9a1121229e82ca29c5771be59f1823cbd2cee7ec
ESPP-ZDT <toplelfaget@gmail.com>
2022-08-19 18:59:33 +0200

    recognised the Navbar error - disabled it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
+//import Navbar from './containers/navbar/Navbar';
 import ProductScreen from './containers/products/ProductScreen';
+//import {BrowserRouter as Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <ProductScreen />
-    </div>
-  );
-}
+    
+      <div className="App">
+          <ProductScreen />
+        
+      </div>
+    
+)}
+
 
 export default App;
